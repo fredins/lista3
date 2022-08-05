7d9439fe7f32c55e344b85027f6feb0727a0390c
Martin Fredin <fredin.martin@gmail.com>
2022-08-05 22:32:16 +0200

    add custom color and style

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lemon: {
+          50: '#FFFEE6',
+          100: '#FFFCC6',
+          200: '#FFFAA8',
+          300: '#FFF784',
+          400: '#FFF568',
+          500: '#FFEF43',
+        },
+      },
+      boxShadow: {
+        'inner-eq': 'inset 0 0 4px 0 rgb(0 0 0 / 0.05)',
+      },
+    },
   },
   plugins: [],
 }
