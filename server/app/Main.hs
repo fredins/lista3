e749ca31ef10651f4e7d5381ccf81b520a6cd1e7
Martin Fredin <fredin.martin@gmail.com>
2022-08-05 15:06:59 +0200

    Add color field

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -13,7 +13,7 @@ import Prelude hiding (id)
 import qualified Prelude ()
 import Servant
 import Servant.Docs (ToSample, toSamples, ToParam, toParam, ToCapture, toCapture, singleSample, DocCapture(DocCapture), DocQueryParam(DocQueryParam), ParamKind(Normal), markdown, docs)
-import Data.Aeson (ToJSON, toJSON, (.=), object, FromJSON, parseJSON, Value(Object), (.:))
+import Data.Aeson (ToJSON, toJSON, (.=), object, FromJSON, parseJSON, Value(Object, String), (.:))
 import GHC.Generics (Generic)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.Cors (corsRequestHeaders, simpleCorsResourcePolicy, cors, corsMethods)
@@ -22,17 +22,20 @@ import Data.UUID.V4 (nextRandom)
 import Data.UUID (nil, toASCIIBytes)
 import Data.UUID.Types (UUID)
 import qualified  Data.ByteString.Lazy.Char8 as LazyByteString (ByteString)
-import Test.QuickCheck (arbitrary, Arbitrary, elements, sample) 
-import Test.QuickCheck.Instances
+import Test.QuickCheck (arbitrary, Arbitrary, elements) 
+import Test.QuickCheck.Instances ()
 import Agda.Utils.Maybe (caseMaybeM)
 import Database.PostgreSQL.Simple (query, query_, execute, execute_, connect, close, Connection, FromRow, ToRow, ConnectInfo, defaultConnectInfo)
 import Database.PostgreSQL.Simple.ToField (ToField, toField, Action(Many, Escape))
+import Database.PostgreSQL.Simple.FromField (FromField, fromField)
 import Database.PostgreSQL.Simple.ToRow (toRow)
 import Control.Monad (void, liftM4, liftM3)
 import Control.Monad.IO.Class (liftIO)
 import Control.Applicative (empty)
 import Data.Pool (Pool, createPool, withResource)
 import Data.Function.Flippers (flip4)
+import Data.Text (unpack, pack)
+import Distribution.Simple.Utils (lowercase)
 
 -- DATA TYPES
 
@@ -40,14 +43,33 @@ data Todo = Todo {
   id :: UUID,
   text :: String, 
   active :: Bool,
-  marked :: Bool
+  color :: Color
 } deriving (Eq, Show, Generic)
+
+data Color = Gray
+           | Blue
+           | Yellow
+           | Red
+           | Pink 
+             deriving (Eq, Generic, Show)
+
+fromString :: String ->Color
+fromString s = case lowercase s of
+  "gray"   -> Gray  
+  "blue"   -> Blue  
+  "yellow" -> Yellow
+  "red"    -> Red   
+  "pink"   -> Pink  
+  err      -> error err
 
 newtype NewTodo = NewTodo (UUID ->Todo) deriving (Generic)
 
 instance Arbitrary Todo where
   arbitrary = liftM4 Todo arbitrary (elements ["Sill", "Dill", "Färskpotatis", 
     "Gräddfil", "Snaps"]) arbitrary arbitrary
+
+instance Arbitrary Color where
+  arbitrary = elements [Gray, Blue, Yellow, Red, Pink]
 
 -- SERVER
 
@@ -59,6 +81,13 @@ type TodoApi = "todos"      :> QueryParam "active" Bool         :> Get  '[JSON] 
 instance ToJSON Todo
 instance FromJSON Todo
 
+instance ToJSON Color where 
+  toJSON = String . pack . lowercase . show
+  
+instance FromJSON Color where
+  parseJSON (String x) = return . fromString $ unpack x
+  parseJSON _     = error "parseJSON Color"
+
 instance ToParam (QueryParam "active" Bool) where
   toParam _ = DocQueryParam  
     "active"  
@@ -67,7 +96,7 @@ instance ToParam (QueryParam "active" Bool) where
     Normal
 
 instance ToSample Todo where 
-  toSamples _ = singleSample $ Todo nil "Sill" True False
+  toSamples _ = singleSample $ Todo nil "Sill" True Gray
  
 instance ToCapture (Capture "id" UUID) where
   toCapture _ =  DocCapture 
@@ -75,14 +104,14 @@ instance ToCapture (Capture "id" UUID) where
     "identification of todo"
 
 instance ToSample (UUID ->Todo) where
-  toSamples _ = singleSample $ \id ->Todo id "Sill" True False
+  toSamples _ = singleSample $ \id ->Todo id "Sill" True Gray
 
 instance ToJSON (UUID -> Todo) where
-  toJSON newTodo = object ["text" .= text, "active" .= active, "marked" .= marked]
+  toJSON newTodo = object ["text" .= text, "active" .= active, "color" .= color]
     where Todo{..} = newTodo nil
 
 instance FromJSON (UUID -> Todo) where
-  parseJSON (Object o) = liftM3 (flip4 Todo) (o .: "marked") (o .: "active") (o .: "text")
+  parseJSON (Object o) = liftM3 (flip4 Todo) (o .: "color") (o .: "active") (o .: "text")
   parseJSON _ = empty
 
 server :: Pool Connection -> Server TodoApi
@@ -107,7 +136,7 @@ server pool =    getTodosByActivity   -- /todos?active=bool
        "Todo with ID not found." :: LazyByteString.ByteString }) return
 
   postNewTodo :: (UUID ->Todo) ->Handler NoContent
-  postNewTodo newTodo = toHandler (`insertTodo` newTodo) >> return NoContent
+  postNewTodo newTodo =  toHandler (`insertTodo` newTodo) >> return NoContent
 
   postUpdateTodo :: Todo ->Handler NoContent
   postUpdateTodo todo = toHandler (`updateTodo` todo) >> return NoContent
@@ -141,7 +170,13 @@ main = do
 
 instance ToField Todo where
   toField Todo{..} = Many [toField id, toField text, toField active, toField
-    marked]
+    color]
+
+instance ToField Color where
+  toField = toField . lowercase . show
+
+instance FromField Color where 
+  fromField f dat = fromString <$> fromField f dat
 
 instance FromRow Todo
 
@@ -156,7 +191,7 @@ initDB conn = void $ execute_ conn
   \   id uuid,                        \
   \   text text,                      \
   \   active boolean not null,        \
-  \   marked boolean not null,        \
+  \   color text,                     \
   \   primary key (id)                \
   \ );"
 
@@ -177,12 +212,12 @@ selectTodoById conn id = listToMaybe <$> query conn
 
 insertTodo :: Connection ->(UUID -> Todo) ->IO ()
 insertTodo conn todo = void $ nextRandom >>= execute conn 
-  "insert into todos values (?, ?, ?, ?)" . todo 
+  "insert into todos values (?, ?, ?, ?)" . todo  
 
 updateTodo :: Connection -> Todo ->IO ()
 updateTodo conn Todo{..} = void $ execute conn 
-  "update todos set id=?, text=?, active=?, marked=? where id=?" (id, text,
-  active, marked, id)
+  "update todos set id=?, text=?, active=?, color=? where id=?" (id, text,
+  active, color, id)
 
 deleteTables :: IO () 
 deleteTables = void $ connect defaultConnectInfo >>= flip execute_"drop table todos"
